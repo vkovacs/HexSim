e0e3f68475fbf97a3fa62c0f6c2fed332d0cdd5a
Viktor Kovacs <viktor.kovacs@siemens.com>
2023-05-16 10:37:49 +0200

    something draen

--- gui/src/main/java/graphics/DrawingPanel.java
+++ gui/src/main/java/graphics/DrawingPanel.java
@@ -9,14 +9,14 @@ import java.awt.Graphics;
 import java.util.List;
 
 public class DrawingPanel extends JPanel {
-    private final HexBoard<Hex> hexBoard = new HexBoard<>(2, 2, Hex.EMPTY);
+    private final HexBoard<Hex> hexBoard = new HexBoard<>(10, 10, Hex.EMPTY);
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        final int size = 12;
-        final int width = 2 * size;
+        final int size = 24;
+        final int width = (int) (Math.sqrt(3) * size);
         final int horizontalSpacing = width;
         final int verticalSpacing = (int) Math.round(size * (3d / 2d));
 
@@ -24,9 +24,9 @@ public class DrawingPanel extends JPanel {
         for (List<Hex> row : hexBoard.getBoard()) {
             int colCounter = 0;
             for (Hex hex : row) {
-                var colOffset = rowCounter % 2 == 1 ? width : size;
+                var colOffset = rowCounter % 2 == 1 ? width : width / 2;
                 var rowOffset = verticalSpacing;
-                HexShape.paint(g, colOffset + colCounter * horizontalSpacing, rowOffset + rowCounter * verticalSpacing , size);
+                HexShape.paint(g, colOffset + colCounter * horizontalSpacing, rowOffset + rowCounter * verticalSpacing, size);
                 colCounter++;
             }
             rowCounter++;
