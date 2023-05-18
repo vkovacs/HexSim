3c6bc84da1cbd8ef8e8b450d6193df254b664fa8
Viktor Kovacs <viktor.kovacs@siemens.com>
2023-05-18 08:03:48 +0200

    run from main source set

--- src/main/java/hu/crs/hexsim/HexSim.java
+++ src/main/java/hu/crs/hexsim/HexSim.java
@@ -1,20 +1,9 @@
 package hu.crs.hexsim;
 
-import hu.crs.hex.HexEntity;
-import hu.crs.hex.HexBoard;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import hu.crs.hex.simulation.Application;
 
-@SpringBootApplication
-public class HexSim implements CommandLineRunner {
+public class HexSim {
     public static void main(String[] args) {
-        SpringApplication.run(HexSim.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-        var hexBoard = new HexBoard<>(3, 4, HexEntity.EMPTY);
-        System.out.println("Hello World " + hexBoard.size());
+        Application.main(args);
     }
 }
