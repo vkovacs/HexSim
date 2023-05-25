49f1dd9e173cef4c9d5175ef93595fba85a2486e
Viktor Kovacs <viktor.kovacs@siemens.com>
2023-05-25 15:01:08 +0200

    stop on mouse click

--- gui/src/main/java/hu/crs/hex/simulation/Application.java
+++ gui/src/main/java/hu/crs/hex/simulation/Application.java
@@ -1,24 +1,28 @@
 package hu.crs.hex.simulation;
 
 import hu.crs.hex.simulation.foxrabbitsimulation.FoxRabbitHexEntity;
-import hu.crs.hex.simulation.foxrabbitsimulation.FoxRabbitPersistentGrassSimulation;
+import hu.crs.hex.simulation.foxrabbitsimulation.FoxRabbitSimulation;
 import hu.crs.hex.simulation.foxrabbitsimulation.FoxRabbitSimulationConfig;
 import hu.crs.hex.simulation.graphics.HexGridPanel;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import static hu.crs.hex.simulation.foxrabbitsimulation.FoxRabbitSimulationConfig.COL_COUNT;
 import static hu.crs.hex.simulation.foxrabbitsimulation.FoxRabbitSimulationConfig.ROW_COUNT;
 
-public class Application extends JFrame {
-    private final Simulation<FoxRabbitHexEntity> simulation = new FoxRabbitPersistentGrassSimulation(ROW_COUNT, COL_COUNT, FoxRabbitHexEntity.EMPTY);
+public class Application extends JFrame implements MouseListener {
+    private final Simulation<FoxRabbitHexEntity> simulation = new FoxRabbitSimulation(ROW_COUNT, COL_COUNT, FoxRabbitHexEntity.EMPTY);
     private final SimulationConfig simulationConfig = new FoxRabbitSimulationConfig();
     private final HexGridPanel<FoxRabbitHexEntity> hexGridPanel = new HexGridPanel<>(simulationConfig.rowCount(), simulationConfig.colCount(), simulationConfig.hexSize(), simulation.hexBoard());
+    private Thread simulationThread;
+    private boolean simulationThreadSuspended = false;
 
     public Application() {
-        setTitle("Hexagon Drawing Example");
+        setTitle("HexSim");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         var screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -26,6 +30,9 @@ public class Application extends JFrame {
         setExtendedState(JFrame.MAXIMIZED_BOTH);
 
         add(hexGridPanel);
+
+        // Register the mouse listener
+        addMouseListener(this);
     }
 
     public static void main(String[] args) {
@@ -36,7 +43,7 @@ public class Application extends JFrame {
     private void start() {
         SwingUtilities.invokeLater(() -> {
             setVisible(true);
-            var simulationThread = new Thread(() -> {
+            simulationThread = new Thread(() -> {
                 while (true) {
                     simulation.step();
                     hexGridPanel.hexBoard(simulation.hexBoard());
@@ -51,5 +58,36 @@ public class Application extends JFrame {
 
             simulationThread.start();
         });
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+        simulationThreadSuspended = !simulationThreadSuspended;
+
+        if (simulationThreadSuspended) {
+            simulationThread.suspend();
+        } else {
+            simulationThread.resume();
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
+
     }
 }
